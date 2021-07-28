2c1616d4033a576a7237f9315128932100c41508
Kodrin <kodrintxps@gmail.com>
2021-07-27 22:14:35 -0400

    creating game manager script

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : Singleton<GameManager>
+{
+    //triggers 
+    public bool hasTriggeredClock = false;
+    public bool hasTriggeredPainting = false;
+    public bool hasTriggeredCereal = false;
+    public bool hasTriggeredMilk = false;
+    
+    
+    
+}
