e18b34f19c623e40a5625209847337e1930820e6
Kodrin <kodrintxps@gmail.com>
2021-07-29 22:03:54 -0400

    encapsulated puzzles into parent puzzle class, added puzzle controller to restrict puzzle intereaction based on camera angle

---
+++ Assets/Scripts/Puzzles/PuzzleController.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleController : MonoBehaviour
+{
+    [SerializeField] protected Puzzle clockPuzzle;
+    [SerializeField] protected Puzzle paintingPuzzle;
+
+    [SerializeField] protected CameraController cameraController;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //todo use camera events to call this , only testing right now
+        //e.g on camera switch
+        if (cameraController.currentCameraShot == CameraShot.GRANDFATHER_SHOT)
+        {
+            EnablePuzzle(clockPuzzle);
+            DisablePuzzle(paintingPuzzle);
+        }
+
+        if (cameraController.currentCameraShot == CameraShot.PAINTING_SHOT)
+        {
+            EnablePuzzle(paintingPuzzle);
+            DisablePuzzle(clockPuzzle);
+            
+        }
+    }
+    
+    
+
+    protected void EnablePuzzle(Puzzle puzzle)
+    {
+        puzzle.canInteract = true;
+    }
+
+    protected void DisablePuzzle(Puzzle puzzle)
+    {
+        puzzle.canInteract = false;
+        
+    }
+}
