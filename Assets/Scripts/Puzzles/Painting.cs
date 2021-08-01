d0fc7ea4cd23abde6e7faf54a4a45b2ac03fe031
Kodrin <kodrintxps@gmail.com>
2021-08-01 18:59:18 -0400

    added win condition to painting puzzle, painting puzzle is done

--- Assets/Scripts/Puzzles/Painting.cs
+++ Assets/Scripts/Puzzles/Painting.cs
@@ -2,17 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public enum PaintingType
+{
+    EYE,
+    BURNING_HOUSE,
+    SELF
+}
+
 public class Painting : MonoBehaviour
 {
-    
-    [System.Serializable]
-    public enum PaintingType
-    {
-        EYE,
-        BURNING_HOUSE,
-        SELF
-    }
+
 
     public PaintingType paintingType;
+    
 
 }
