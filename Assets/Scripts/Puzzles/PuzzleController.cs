89d27d3d9f21a7afb7fbdb51c76db82bc3eb57e2
Kodrin <kodrintxps@gmail.com>
2021-08-03 23:16:14 -0400

    updated puzzle controller to check for puzzle interaction only on camera switching event

--- Assets/Scripts/Puzzles/PuzzleController.cs
+++ Assets/Scripts/Puzzles/PuzzleController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,33 +8,38 @@ public class PuzzleController : MonoBehaviour
     [SerializeField] protected Puzzle clockPuzzle;
     [SerializeField] protected Puzzle paintingPuzzle;
     
-    
-    // Start is called before the first frame update
-    protected void Start()
+
+    protected void OnEnable()
     {
-        
+        EventManager.Instance.OnCameraSwitch += CheckPuzzleControls; //add so it will enable interaction when camera is switching
     }
 
-    // Update is called once per frame
-    protected void Update()
+    protected void OnDisable()
     {
-        //todo use camera events to call this , only testing right now
-        //e.g on camera switch
-        // if (CameraController.Instance.currentCameraShot.shotType == ShotType.GRANDFATHER_SHOT)
-        // {
-        //     EnablePuzzle(clockPuzzle);
-        //     DisablePuzzle(paintingPuzzle);
-        // }
-        //
-        // if (CameraController.Instance.currentCameraShot.shotType == ShotType.PAINTING_SHOT)
-        // {
-        //     EnablePuzzle(paintingPuzzle);
-        //     DisablePuzzle(clockPuzzle);
-        //     
-        // }
+        EventManager.Instance.OnCameraSwitch -= CheckPuzzleControls; //add so it will enable interaction when camera is switching
     }
-    
-    
+
+    protected void CheckPuzzleControls()
+    {
+        ShotType currentShotType = CameraController.Instance.currentCameraShot.shotType;
+        
+        if (currentShotType == ShotType.GRANDFATHER_SHOT)
+        {
+            EnablePuzzle(clockPuzzle);
+            DisablePuzzle(paintingPuzzle);
+        }
+        else if (currentShotType == ShotType.PAINTING_SHOT)
+        {
+            //todo need to fix the can interact bool on the coroutine so it doesnt conflict with this 
+            EnablePuzzle(paintingPuzzle);
+            DisablePuzzle(clockPuzzle);
+        }
+        else
+        {
+            DisablePuzzle(paintingPuzzle);
+            DisablePuzzle(clockPuzzle);
+        }
+    }
 
     protected void EnablePuzzle(Puzzle puzzle)
     {
