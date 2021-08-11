a4566ad64a35bbcfe8106665bc2d66a933ebf72c
jeremydecola <jeremy.decola-david@mail.mcgill.ca>
2021-08-10 21:42:33 -0400

    added linebreaks for note interaction

--- Assets/Scripts/Note.cs
+++ Assets/Scripts/Note.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 [CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/Note", order = 1)]
 public class Note : ScriptableObject
